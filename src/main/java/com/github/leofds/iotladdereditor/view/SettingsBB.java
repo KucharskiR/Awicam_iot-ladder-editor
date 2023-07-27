24c6dc41febf6e124b6a8238a685289bbbb93494
KucharskiR <kucharski.radek@gmail.com>
2023-07-27 09:10:05 +0200

    Add save and cancel buttons

--- src/main/java/com/github/leofds/iotladdereditor/view/SettingsBB.java
+++ src/main/java/com/github/leofds/iotladdereditor/view/SettingsBB.java
@@ -3,6 +3,12 @@ package com.github.leofds.iotladdereditor.view;
 import java.awt.Dimension;
 
 import javax.swing.JDialog;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SettingsBB extends JDialog{
 
@@ -11,7 +17,7 @@ public class SettingsBB extends JDialog{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-//	private final JPanel contentPanel = new JPanel();
+	private final JPanel panel = new JPanel();
 
 	public SettingsBB() {
 		super();
@@ -25,7 +31,37 @@ public class SettingsBB extends JDialog{
 			setResizable(true);
 			
 			setBounds(100, 100, 400, 500);
-//			getContentPane().setLayout(new BorderLayout());
+			getContentPane().setLayout(null);
+			
+//			JPanel panel = new JPanel();
+			getContentPane().add(panel, BorderLayout.CENTER);
+			
+			JButton btnSave = new JButton("Save");
+			btnSave.setBounds(229, 427, 57, 23);
+			getContentPane().add(btnSave);
+			
+			JButton btnCancel = new JButton("Cancel");
+			btnCancel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					SettingsBB.this.dispose();
+				}
+			});
+			btnCancel.setBounds(293, 427, 65, 23);
+			getContentPane().add(btnCancel);
+			
+			JCheckBox chckbxSetting = new JCheckBox("Setting 1");
+			chckbxSetting.setBounds(20, 17, 97, 23);
+			getContentPane().add(chckbxSetting);
+			
+			JCheckBox chckbxSetting_1 = new JCheckBox("Setting 2");
+			chckbxSetting_1.setBounds(20, 43, 97, 23);
+			getContentPane().add(chckbxSetting_1);
+			
+			JCheckBox chckbxSetting_2 = new JCheckBox("Setting 3");
+			chckbxSetting_2.setBounds(20, 69, 97, 23);
+			getContentPane().add(chckbxSetting_2);
+			
+			
 //			Border border = BorderFactory.createTitledBorder("Settings");
 //			contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 //			contentPanel.setBorder(border);
@@ -38,5 +74,4 @@ public class SettingsBB extends JDialog{
 		
 		
 	}
-
 }
