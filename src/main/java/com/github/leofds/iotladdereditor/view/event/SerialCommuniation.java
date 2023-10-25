653decc0c85effe0499cacb77552d8729fed5369
KucharskiR-Awicam <rkucharski.awicam@gmail.com>
2023-10-25 14:51:51 +0200

    Update SerialCommunication

--- src/main/java/com/github/leofds/iotladdereditor/view/event/SerialCommuniation.java
+++ src/main/java/com/github/leofds/iotladdereditor/view/event/SerialCommuniation.java
@@ -3,7 +3,11 @@ package com.github.leofds.iotladdereditor.view.event;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import com.github.leofds.iotladdereditor.application.Mediator;
 
 import gnu.io.CommPortIdentifier;
 import gnu.io.NoSuchPortException;
@@ -19,6 +23,9 @@ public class SerialCommuniation {
 	private static final int USB_ESP_OK = 0x16;
 	private static final int USB_ESP_ERROR = 0x17;
 	
+	private static enum Error {ERROR_RECEIVED, ERROR_SEND};
+	private static enum Success {SUCCESS_RECEIVED, SUCCESS_SEND};
+	
 	private String portName;
 
 	public SerialCommuniation(String portName) {
@@ -26,33 +33,76 @@ public class SerialCommuniation {
 	}
 	
 	public void send() {
-		String fileName = System.getProperty("user.dir");
+		String fileName = System.getProperty("user.dir") + "file.ld";
+		
+		Thread send = new Thread(() -> {
+			try {
+				CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+				SerialPort serialPort = (SerialPort) portIdentifier.open("SerialCommuniation", 2000);
 
-		try {
-			CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-			SerialPort serialPort = (SerialPort) portIdentifier.open("SerialCommuniation", 2000);
+				serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+						SerialPort.PARITY_NONE);
 
-			serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+				File file = new File(fileName);
+				FileInputStream fileInputStream = new FileInputStream(file);
+				InputStream inputStream = serialPort.getInputStream();
+				OutputStream outputStream = serialPort.getOutputStream();
+				
+				byte[] buffer = new byte[64];
+				int len;
+				while ((len = fileInputStream.read(buffer)) > 0) {
+					outputStream.write(buffer, 0, len);
+					try {
+						wait(5);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						error(Error.ERROR_SEND);
+					}
+				}
+				
+				// Send END OF DATA 
+				outputStream.write(END_OF_DATA);
+				
+				try {
+					wait(1);
+					
+				    int availableBytes = inputStream.available();
+		            byte[] bufferIn = new byte[availableBytes];
+		            int bytesRead = ByteBuffer.wrap(bufferIn).getInt();
+		            
+		            
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					error(Error.ERROR_RECEIVED);
+				}
+				
+				// Close connection
+				fileInputStream.close();
+				outputStream.close();
+				serialPort.close();
 
-			File file = new File(fileName);
-			FileInputStream fileInputStream = new FileInputStream(file);
-			OutputStream outputStream = serialPort.getOutputStream();
+				success(Success.SUCCESS_SEND);
 
-			byte[] buffer = new byte[64];
-			int len;
-			while ((len = fileInputStream.read(buffer)) > 0) {
-				outputStream.write(buffer, 0, len);
+			} catch (NoSuchPortException | PortInUseException | IOException | UnsupportedCommOperationException e) {
+				e.printStackTrace();
 			}
-
-			fileInputStream.close();
-			outputStream.close();
-			serialPort.close();
-
-			System.out.println("File sent successfully.");
-
-		} catch (NoSuchPortException | PortInUseException | IOException | UnsupportedCommOperationException e) {
-			e.printStackTrace();
-		}
+		}); // End of thread
+		
+		// Start thread
+		send.start();
 	}
-
+	
+	private void error(Error error) {
+		//TODO: Error procedure to write
+		consoleOutput("error");
+	}
+	
+	private void success(Success success) {
+		//TODO: Success procedure to write
+		consoleOutput("success");
+	}
+	
+	private static void consoleOutput(String msg) {
+		Mediator.getInstance().outputConsoleMessage(msg);
+	}
 }
